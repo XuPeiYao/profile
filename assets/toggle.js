7cb87e61e5c9405d7c8001921cd92c6ebacae9a0
徐培堯 <st89382000@gmail.com>
2016-10-06 23:33:26 +0800

    Init

---
+++ assets/toggle.js
@@ -0,0 +1,26 @@
+$(function() {
+    var toggleFloatingMenu = function() {
+        $('.js-floating-nav').toggleClass('is-visible');
+        $('.js-floating-nav-trigger').toggleClass('is-open');
+    };
+
+    $(".background-card").css("min-height", window.screen.availHeight + "px");
+    $("[data-toggle=tooltip]").tooltip();
+    $('.js-floating-nav-trigger').on('click', function(e) {
+        e.preventDefault();
+        toggleFloatingMenu();
+    });
+    $('.js-floating-nav a').on('click', toggleFloatingMenu);
+
+    $("#remaining-profiles").on('show.bs.collapse', function() {
+        $('.js-profiles-collapse > i')
+            .removeClass('icon-chevron-down')
+            .addClass('icon-chevron-up');
+    });
+
+    $("#remaining-profiles").on('hidden.bs.collapse', function() {
+        $('.js-profiles-collapse > i')
+            .removeClass('icon-chevron-up')
+            .addClass('icon-chevron-down');
+    });
+});
