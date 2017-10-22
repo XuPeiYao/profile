6bc45da2ab3f63b7341bfb6443d3d62facd68beb
徐培堯 <st89382000@gmail.com>
2017-10-22 13:53:32 +0800

    左方導覽列串接路由

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,43 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { WorkComponent } from './pages/work/work.component';
+import { EducationComponent } from './pages/education/education.component';
+import { PublicationsComponent } from './pages/publications/publications.component';
+import { SkillsComponent } from './pages/skills/skills.component';
+import { AwardsComponent } from './pages/awards/awards.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'about',
+    pathMatch: 'full'
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'work',
+    component: WorkComponent
+  },
+  {
+    path: 'education',
+    component: EducationComponent
+  },
+  {
+    path: 'publications',
+    component: PublicationsComponent
+  },
+  {
+    path: 'skills',
+    component: SkillsComponent
+  },
+  {
+    path: 'awards',
+    component: AwardsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
