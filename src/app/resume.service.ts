3469d13dbae08b58c18bc356c080bb4b676678dc
徐培堯 <st89382000@gmail.com>
2017-10-22 15:53:21 +0800

    ResumeService支援列隊等候，防止重複請求

--- src/app/resume.service.ts
+++ src/app/resume.service.ts
@@ -6,6 +6,8 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class ResumeService {
   private cache: any;
+  private locked: boolean;
+  private queue: ((result: any) => void)[] = [];
   constructor(private http: Http) { }
 
   public async getResume(): Promise<any> {
@@ -14,12 +16,21 @@ export class ResumeService {
         res(this.cache);
         return;
       }
+      if (this.locked) {
+        this.queue.push(res);
+        return;
+      }
+      this.locked = true; // 鎖定
       this.http
         .get('assets/resume.json')
         .map(x => x.json())
         .subscribe(x => {
           this.cache = x;
           res(x);
+          for (const item of this.queue){
+            item(x);
+          }
+          this.queue = [];
         });
     });
   }
