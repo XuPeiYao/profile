2e101e9335da82060dde05dc9ccee67105c98b11
徐培堯 <st89382000@gmail.com>
2017-10-22 13:53:13 +0800

    加入路由頁面組件

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,25 @@ import { AppComponent } from './app.component';
 import { AvatarComponent } from './avatar/avatar.component';
 import { UserCardComponent } from './user-card/user-card.component';
 import { MenuComponent } from './menu/menu.component';
+import { AboutComponent } from './pages/about/about.component';
+import { WorkComponent } from './pages/work/work.component';
+import { SkillsComponent } from './pages/skills/skills.component';
+import { EducationComponent } from './pages/education/education.component';
+import { AwardsComponent } from './pages/awards/awards.component';
+import { PublicationsComponent } from './pages/publications/publications.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AvatarComponent,
     UserCardComponent,
-    MenuComponent
+    MenuComponent,
+    AboutComponent,
+    WorkComponent,
+    SkillsComponent,
+    EducationComponent,
+    AwardsComponent,
+    PublicationsComponent,
   ],
   imports: [
     BrowserModule,
