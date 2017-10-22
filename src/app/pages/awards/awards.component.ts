f93a594136909eb20e41bc550a98164085cc2aa9
徐培堯 <st89382000@gmail.com>
2017-10-22 15:10:24 +0800

    完成獎項頁面

--- src/app/pages/awards/awards.component.ts
+++ src/app/pages/awards/awards.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ResumeService } from '../../resume.service';
 
 @Component({
   selector: 'app-awards',
@@ -6,10 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./awards.component.css']
 })
 export class AwardsComponent implements OnInit {
+  @Input() profile;
+  constructor(private resumeService: ResumeService) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  async ngOnInit() {
+    this.profile = await this.resumeService.getResume();
   }
-
 }
