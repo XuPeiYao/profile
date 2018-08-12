54acd7449a6698acb71ab85d2cb46d84188ce423
XuPeiYao <st89382000@gmail.com>
2018-08-12 23:14:07 +0800

    更新自傳與技能等資訊

--- src/app/pages/publications/publications.component.ts
+++ src/app/pages/publications/publications.component.ts
@@ -14,29 +14,30 @@ export class PublicationsComponent implements OnInit {
   constructor(
     private resumeService: ResumeService,
     private githubService: GithubService
-    ) { }
+  ) { }
 
   async ngOnInit() {
     this.profile = await this.resumeService.getResume();
     this.loading = true;
-    this.profile.publications = this.profile.publications.map(x => {
-        x.releaseDate = new Date(x.releaseDate);
-        return x;
-      }).concat(
+    this.profile.publications =
       (await this.githubService.getAllRepos())
         .filter(x => x.stargazers_count)
         .map(x => {
           return {
-            name : x.name,
-            releaseDate : new Date(x.created_at),
+            name: x.name,
+            releaseDate: new Date(x.created_at),
             website: x.html_url,
             summary: x.description
           };
         })
         .sort((a: any, b: any) => {
           return b.releaseDate - a.releaseDate;
-        })
-    );
+        }).concat(
+          this.profile.publications.map(x => {
+            x.releaseDate = new Date(x.releaseDate);
+            return x;
+          })
+        );
     this.loading = false;
   }
 }
